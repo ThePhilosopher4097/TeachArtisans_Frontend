fef55e5dbe8f9f4c78ca5a72db36a891ced939b6
preetanam123 <80145584+preetanam123@users.noreply.github.com>
2022-11-12 20:02:47 +0530

    tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    color: {
+
+      'blue': '#2E4FFF',
+      'orange': '#FD8A37',
+      
+    },
+    extend: {},
+  },
+  plugins: [],
+}
